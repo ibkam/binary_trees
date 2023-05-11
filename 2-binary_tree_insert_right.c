eae00b71645b563d155ccce1881fed0d520219e4
ibkam <110990848+ibkam@users.noreply.github.com>
2023-05-11 08:42:31 +0300

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = parent->right;
 	if (new_node->right)
-		parents->right->parent =new_node;
+		parents->right->parent = new_node;
 	parent->right = new_node;
 	return (new_node);
 }
