68cb452b4dded0f84877905ab7c8054afddf9c6a
ibkam <110990848+ibkam@users.noreply.github.com>
2023-05-15 10:57:34 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,11 +6,9 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
-	
 	if (tree)
 	{
+		size_t l = 0, r = 0;
 		l = tree->left + binary_tree_height(tree->left);
 		r = tree->right + binary_tree_height(tree->right);
 		return (l > r ? l : r);
