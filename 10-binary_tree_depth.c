2ce62d2d916cdae85f5af0d9a3e742b87891f8fc
ibkam <110990848+ibkam@users.noreply.github.com>
2023-05-22 09:46:33 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,7 +4,7 @@
  * @tree: pointer to the node.
  * Return: 0 if the tree is NULL
  */
- size_t binary_tree_depth(const binary_tree_t *tree)
- {
-	 return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
- }
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
