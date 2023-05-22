674503f65febe141ab2392d20e0f0e8aaa50b0ba
ibkam <110990848+ibkam@users.noreply.github.com>
2023-05-22 10:56:10 +0300

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,17 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - mmeasures the size of a binary tree
- * @tree: pointer to the node.
- * Return: 0 if the tree is NULL
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: If the tree is NULL, must return 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;
-	
+
 	if (tree)
 	{
 		size += 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
 	}
 	return (size);
 }
