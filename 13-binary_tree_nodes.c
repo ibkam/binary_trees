37ccdea379037bd364e6fd691304176e29c4d9c8
ibkam <110990848+ibkam@users.noreply.github.com>
2023-05-22 11:05:24 +0300

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
- * @tree: A pointer to the root node of the tree to count the number of nodes.
+ * binary_tree_nodes - a function that returns nodes with atleast one child
+ * @tree: the root node
  *
- * Return: If tree is NULL, the function must return 0, else return node count.
+ * Return: the number of nodes with atleast one child
  */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
@@ -14,34 +15,32 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_size - mmeasures the size of a binary tree
- * @tree: pointer to the node.
- * Return: 0 if the tree is NULL
+ * binary_tree_size - a function that returns the size of a binary tree
+ * @tree: the root node
+ *
+ * Return: the number of nodes in the tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
-	
-	if (tree)
-	{
-		size += 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
-	}
-	return (size);
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: pointer to the node.
- * Return: 0 if the tree is NULL.
+ * binary_tree_leaves - a function to determine the number of leaves in a tree
+ * @tree: the root node
+ *
+ * Return: the number of leaves
  */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t Leaves = 0;
-	
-	if (tree)
-	{
-		Leaves += ((!tree->left && !tree->right) ? 1 : 0);
-		Leaves += binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-	}
-	return (Leaves);
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
