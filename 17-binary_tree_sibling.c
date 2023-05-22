a32380bb3af37894aaa03e847bf6e8746c689e27
ibkam <110990848+ibkam@users.noreply.github.com>
2023-05-22 08:28:03 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->left == node->parent)
+	if (node->parent->left == node)
 		return (node->parent->right);
 	return (node->parent->left);
 }
